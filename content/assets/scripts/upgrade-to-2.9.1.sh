0069ed3426cd6dc1717a0bb56dfb188d7e5a32cf
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-11-12 13:02:40 +0100

    check the version of PHP before updateing MONARC.

--- content/assets/scripts/upgrade-to-2.9.1.sh
+++ content/assets/scripts/upgrade-to-2.9.1.sh
@@ -1,11 +1,56 @@
 #! /usr/bin/env bash
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 # Everything must be executed with the normal user.
 if [[ $EUID -eq 0 ]]; then
-    echo "This script must not be executed with root privileges." 2>&1
+    echo "${RED}This script must not be executed with root privileges.${NC}" 2>&1
     exit 1
 fi
+
+
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+PHP_VERSION=`php -v | grep ^PHP | cut -d ' ' -f2 | cut -d '-' -f1`
+
+
+if [[ vercomp $PHP_VERSION 7.2.0 -eq 2  ]]
+    echo -e "${RED}You are using an old version of PHP: $PHP_VERSION${NC}"
+    echo "The migration should works but we advise you to update."
+    echo "https://www.php.net/supported-versions.php"
+    sleep 3
+fi
+
 
 # Clean old directories
 rm -Rf vendor/
