2a4093d933d992337e70624d6e96d23db929c31b
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-11-12 08:28:31 +0100

    added upgrade script for release 2.9.1 of MONARC.

---
+++ content/assets/scripts/upgrade-to-2.9.1.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+
+# Everything must be executed with the normal user.
+if [[ $EUID -eq 0 ]]; then
+  echo "This script must not be executed with root privileges." 2>&1
+  exit 1
+fi
+
+# Clean old directories
+rm -Rf vendor/
+rm -Rf module/
+rm -Rf data/cache
+
+# Create new required directories
+mkdir -p module/Monarc
+mkdir -p data/cache
+mkdir -p data/DoctrineORMModule
+mkdir -p data/LazyServices/Proxy
+
+# Updated code MONARC PHP libraries and dependencies
+composer install -o
+
+# Create new symlinks
+cd module/Monarc
+ln -s ./../../vendor/monarc/core Core
+ln -s ./../../vendor/monarc/frontoffice FrontOffice
+
+# Update all the project
+./scripts/update-all.sh -c
+
+exit 0
